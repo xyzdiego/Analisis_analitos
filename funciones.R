969fc8959bc0bff128dcd5343c485d5b4142744c
Diego Andres Benitez <65202597+xyzdiego@users.noreply.github.com>
2024-03-07 15:01:54 -0500

    Add files via upload
    Version paper

--- funciones.R
+++ funciones.R
@@ -1,24 +1,24 @@
-# Funciones para el análisis gráfico de la base de datos de unas plantas
-# en relación a los genotipos estudiados dentro de una tesis de 
-# maestría.
+# Funciones para el an?lisis gr?fico de la base de datos de unas plantas
+# en relaci?n a los genotipos estudiados dentro de una tesis de 
+# maestr?a.
 #
-# Este código fue elaborado por Diego Andres Benitez.
+# Este c?digo fue elaborado por Diego Andres Benitez.
 #
-# La función Selector, sirve como base de las demás funciones. Permite
-# la generación de una base de datos ordenada lista para el análisis gráfico
-# que realizará EDA_boxplot(), o el análisis de diferencias que realizará
+# La funci?n Selector, sirve como base de las dem?s funciones. Permite
+# la generaci?n de una base de datos ordenada lista para el an?lisis gr?fico
+# que realizar? EDA_boxplot(), o el an?lisis de diferencias que realizar?
 # test_dif().
-# La función EDA_boxplot() realiza un Análisis Exploratorio de Datos (EDA)
+# La funci?n EDA_boxplot() realiza un An?lisis Exploratorio de Datos (EDA)
 # del cual se obtiene un boxplot resultado de acuerdo a cada componente
 # o analito del estudio de maiz.
-# La función IC_teorico() actua teniendo como base la función EDA_boxplot(),
-# pero permite al usuario añadir el intervalo de confianza teórico sobre
-# el cual debería encontrarse cada analito.
-# La función test_dif() actua teniendo como base la función Selector(), con
-# esa información permite realizar un análisis de diferencias entre genotipos
+# La funci?n IC_teorico() actua teniendo como base la funci?n EDA_boxplot(),
+# pero permite al usuario a?adir el intervalo de confianza te?rico sobre
+# el cual deber?a encontrarse cada analito.
+# La funci?n test_dif() actua teniendo como base la funci?n Selector(), con
+# esa informaci?n permite realizar un an?lisis de diferencias entre genotipos
 # de acuerdo con el analito a trabajar. 
 
-Selector <- function(datos = semilla, i = i, variable = "SEMILLA"){
+Selector <- function(datos = semilla, i = i, variable = "grain"){
     datos[i, ] <- sub(",", ".", datos[i, ], fixed = T)
     nombre <<- datos[i, 1]
     datos[i, ] <- as.numeric(datos[i, 1:length(datos[1, ])])
@@ -28,53 +28,92 @@ Selector <- function(datos = semilla, i = i, variable = "SEMILLA"){
     ordenado <- ordenado %>% separate(col = LINEA, 
                                       into = c("Tipo", "Subtipo", "Linea"), 
                                       sep = " ") %>%
-        unite(col = Tipo, sep = "_", Tipo, Subtipo)
+        unite(col = Tipo, sep = "", Tipo, Subtipo)
     ordenado$Tipo <- as.factor(ordenado$Tipo)
     ordenado$Valor <- as.numeric(ordenado$Valor)
     return(ordenado)
 }
 
-EDA_boxplot <- function(datos = semilla, i = i, variable = "SEMILLA"){
-    if(!require(dplyr) & !require(tidyr) & !require(ggplot2)){
-        stop("Por favor instale los paquetes dplyr y tidyr")
+EDA_boxplot <- function(datos = semilla, i = i, variable = "grain"){
+    if(!require(dplyr) & !require(tidyr) & !require(ggplot2) &
+       !require(stringr) & !require(tokenizers)){
+        stop("Por favor instale los paquetes tidyverse y stringr")
     }
     ordenado <- Selector(datos, i, variable)
-    cla1 <- c("#1F618D", "#A04000", "#2E86C1", "#AED6F1", "#D35400", "#EDBB99")
-    g1 <<- ordenado %>% ggplot(aes(Tipo, Valor, fill = Tipo)) + 
-        geom_boxplot() + stat_boxplot(geom ='errorbar', width = 0.5, alpha = 0.3) +
+    cla1 <- c("gray16", "gray32", "gray48", "gray64", "gray80", "gray96")
+    g1 <<- ordenado %>% 
+        mutate(Tipo = fct_relevel(Tipo, 
+                      "HibrydC", "HibrydT", "C1", "C2", "T1", "T2")) %>% 
+        ggplot(aes(Tipo, Valor, fill = Tipo)) + 
+        stat_boxplot(geom ='errorbar') + geom_boxplot() +
         theme_bw() + scale_fill_manual(values = cla1) +
-        labs(x = "GENOTIPO",
-             y = nombre,
-             title = paste(nombre, "DEL", variable, "DE ACUERDO AL GENOTIPO")) +
+        labs(x = "Genotype",
+             y = str_to_title(nombre) %>% sub("Dw", "DW", .),
+             title = sub("(%DW)|(%)", c(""), nombre) %>% 
+               str_extract_all("[:alpha:]+") %>% unlist() %>% 
+               paste(collapse = " ") %>% 
+               str_to_title(),
+             caption = "AFSI, 2020") +
         theme(legend.position = "none",
               plot.title = element_text(hjust = 0.5, color = "gray25",
-                                        size =  18, face = "bold"),
+                                        size =  14, face = "bold"),
+              plot.caption = element_text(hjust = 1, color = "gray25",
+                                        size =  8),
               axis.title.x = element_text(color = "gray25"),
-              axis.title.y = element_text(color = "gray25"))
+              axis.title.y = element_text(color = "gray25"),
+              axis.text.x = element_text(angle = 45))
 }
 
-IC_teorico <- function(datos = semilla, i = 1, variable = "SEMILLA",
+IC_teorico <- function(datos = semilla, i = 1, variable = "grain",
                   min = 0, max = 15){
+    if(!require(knitr) & !require(tidyr)){
+        stop("Por favor instale los paquetes knitr y car")
+    }
     EDA_boxplot(datos = datos, i = i, variable = variable)
     ICG <- data.frame(minimo = rep(min, 18),
                       maximo = rep(max, 18))
-    g1 + geom_errorbar(aes(ymin=ICG$minimo, ymax=ICG$maximo), 
-                       width=0.2, size=1.0, alpha=0.05)
+    grafico <- g1 + geom_hline(yintercept = ICG$minimo, color = "black",
+                               size = 1.0, alpha = 0.45) +
+        geom_text(x = 3.5, y = ICG$minimo + 0.15, 
+                  label = "Minimum range (Literature)", 
+                  color = "gray50", size = 3) +
+        geom_hline(yintercept = ICG$maximo, color = "black",
+                   size = 1.0, alpha = 0.45) +
+        geom_text(x = 3.5, y = ICG$maximo - 0.2, 
+                  label = "Maximum range (Literature)", 
+                  color = "gray50", size = 3)
+    Tabla <- Selector(i = i, datos = datos) %>% group_by(Tipo) %>% 
+            summarise(Media = round(mean(Valor),2),
+                      Desv = round(sd(Valor),3),
+                      Mediana = median(Valor),
+                      Max_rep = max(Valor),
+                      Min_rep = min(Valor),
+                      IQR_max = Mediana + IQR(Valor),
+                      IQR_min = Mediana - IQR(Valor),
+                      Min_lit = min,
+                      Max_lit = max) %>%
+            mutate(Tipo = fct_relevel(Tipo, 
+            "HibrydC", "HibrydT", "C1", "C2", "T1", "T2")) %>% 
+            kable()
+    mediana <<- list(grafico = grafico,
+                    Tabla = Tabla,
+                    nombre = nombre)
 }
 
-test_dif <- function(datos = semilla, i = i, variable = "SEMILLA"){
+test_dif <- function(datos = semilla, i = i, variable = "grain"){
     if(!require(knitr) & !require(car)){
-        stop("Por favor instale los paquetes dplyr y tidyr")
+        stop("Por favor instale los paquetes knitr y car")
     }
-    datos <- Selector(datos = semilla, i = i, variable = "SEMILLA")
+    datos <- Selector(datos = semilla, i = i, variable = "grain")
     Homocedasticidad <- leveneTest(datos$Valor ~ datos$Tipo, center = "median")
     kw_test <- kruskal.test(datos$Valor ~ datos$Tipo)
-    pos_hoc <- pairwise.wilcox.test(datos$Valor, datos$Tipo, p.adjust.method = "holm")
+    pos_hoc <- pairwise.wilcox.test(datos$Valor, datos$Tipo, 
+                                    p.adjust.method = "bonf")
     tabla <- kable(pos_hoc$p.value)
     salida <<- list(nombre = nombre,
                     Homocedasticidad = Homocedasticidad, 
                     kw_test = kw_test, 
                     pos_hoc = pos_hoc, 
                     tabla = tabla)
-    message('El argumento "salida$" le permitira tener acceso a los elementos del análisis')
+    message('El argumento "salida" le permitira tener acceso a los elementos del an?lisis')
 }
--- workflow.R
+++ workflow.R
@@ -1,11 +1,14 @@
 library(tidyr)
 library(dplyr)
 library(ggplot2)
+library(forcats)
+library(knitr)
+library(stringr)
 source("funciones.R")
 
-plantas <- read.csv("DATA_EQS.csv", encoding = "UTF-8")
+plantas <- read.csv2("DATA_EQS.csv", encoding = "UTF-8")
 
-y <- c("Componente", rep(c(rep("LINEA_", 6), rep("HIBRIDO_", 3)), 2))
+y <- c("Componente", rep(c(rep("", 6), rep("Hibryd ", 3)), 2))
 w <- c("", rep("T", 9), rep("C", 9))
 y <- paste0(y, w)
 z <- c("", rep(c(paste(1, LETTERS[1:3]), paste(2, LETTERS[1:3]), 
@@ -20,7 +23,135 @@ forraje <- plantas[1:11, ]
 forraje <- forraje[2:11, ]
 names(forraje) <- componente
 
-IC_teorico(i = 4, min = 11, max = 18)
-test_dif(i = 4)
+IC_teorico(i = 2, datos = forraje, min = 5.72, max = 17.26)
+IC_teorico(i = 6, min = 9.72, max = 14.26)
+mediana$grafico
+
+grain <- data.frame(min = c(7, 5.72, 1.363, 0.49, 0.616, 0, 77.4), 
+                    max = c(23, 17.26, 7.83, 5.5, 6.282, 0, 89.7))
+
+myplots <- vector('list', 7)
+
+for (i in c(1:5,7)) {
+  message(i)
+  myplots[[i]] <- local({
+    i <- i
+    p1 <- IC_teorico(i=i, min = grain[i,1], max = grain[i,2])
+    print(p1)
+  })
+}
+
+cowplot::plot_grid(myplots[[1]]$grafico,
+                   myplots[[2]]$grafico,
+                   myplots[[3]]$grafico,
+                   myplots[[4]]$grafico,
+                   myplots[[5]]$grafico,
+                   myplots[[7]]$grafico, nrow = 3,
+                   labels = "AUTO")
+
+
+forage <- data.frame(min = c(55.3, 3.14, 1.04, 12.5, 0.66, 0,0,0,0, 66.9),
+                     max = c(87.1, 16.32, 6.755, 42, 13.2, 0,0,0,0, 92.9))
+
+myplots <- vector('list', 10)
+
+for (i in c(1:5,10)) {
+  message(i)
+  myplots[[i]] <- local({
+    i <- i
+    p1 <- IC_teorico(datos = forraje, variable = "forage",
+                     i=i, min = forage[i,1], max = forage[i,2])
+    print(p1)
+  })
+}
+
+cowplot::plot_grid(myplots[[1]]$grafico,
+                   myplots[[2]]$grafico,
+                   myplots[[3]]$grafico,
+                   myplots[[4]]$grafico,
+                   myplots[[5]]$grafico,
+                   myplots[[10]]$grafico, nrow = 3,
+                   labels = "AUTO")
+
+mediana$Tabla
+mediana$grafico
+test_dif(i = 2)
 salida$tabla
 salida$nombre
+salida$kw_test
+
+IC_teorico(datos = forraje, i = 3, variable = "FORRAJE")
+test_dif(datos = forraje, i = 3, variable = "FORRAJE")
+salida$tabla
+salida$nombre
+
+for(i in 1:nrow(semilla)){
+    semilla[i, ] <- sub(",", ".", semilla[i, ], fixed = T)
+    nombre <- semilla[i, 1]
+    semilla[i, ] <- as.numeric(semilla[i, 1:length(semilla[1, ])])
+    semilla[i, 1] <- nombre
+}
+semilla
+
+ordenado <- semilla %>% gather(key = "LINEA", value = "Valor", 2:19) %>%
+    arrange(desc(LINEA))
+ordenado <- ordenado %>% separate(col = LINEA, 
+                                  into = c("Tipo", "Subtipo", "Linea"), 
+                                  sep = " ") %>%
+    unite(col = Tipo, sep = "", Tipo, Subtipo)
+ordenado$Tipo <- as.factor(ordenado$Tipo)
+ordenado$Valor <- as.numeric(ordenado$Valor)
+kruskal.test(ordenado$Valor ~ ordenado$Tipo)
+pairwise.wilcox.test(ordenado$Valor, ordenado$Tipo, p.adjust.method = "holm")
+cla1 <- c("#1F618D", "#A04000", "#2E86C1", "#AED6F1", "#D35400", "#EDBB99")
+p1 <- ordenado %>% 
+    mutate(Tipo = fct_relevel(Tipo, 
+                              "HibrydC", "HibrydT", "C1", "C2", "T1", "T2")) %>% 
+    ggplot(aes(Tipo, Valor, fill = Tipo)) + 
+    geom_boxplot() + stat_boxplot(geom ='errorbar', width = 0.5, alpha = 0.3) +
+    theme_bw() + scale_fill_manual(values = cla1) +
+    labs(x = "GENOTIPO",
+         y = "ANALITOS",
+         title = paste("ANALITOS", "DE", "GRANO", "DE ACUERDO AL GENOTIPO")) +
+    theme(legend.position = "none",
+          plot.title = element_text(hjust = 0.5, color = "gray25",
+                                    size =  18, face = "bold"),
+          axis.title.x = element_text(color = "gray25"),
+          axis.title.y = element_text(color = "gray25"))
+
+
+
+for(i in 1:nrow(forraje)){
+    forraje[i, ] <- sub(",", ".", forraje[i, ], fixed = T)
+    nombre <- semilla[i, 1]
+    forraje[i, ] <- as.numeric(forraje[i, 1:length(forraje[1, ])])
+    forraje[i, 1] <- nombre
+}
+
+ordenado <- forraje %>% gather(key = "LINEA", value = "Valor", 2:19) %>%
+    arrange(desc(LINEA))
+ordenado <- ordenado %>% separate(col = LINEA, 
+                                  into = c("Tipo", "Subtipo", "Linea"), 
+                                  sep = " ") %>%
+    unite(col = Tipo, sep = "", Tipo, Subtipo)
+ordenado$Tipo <- as.factor(ordenado$Tipo)
+ordenado$Valor <- as.numeric(ordenado$Valor)
+kruskal.test(ordenado$Valor ~ ordenado$Tipo)
+pairwise.wilcox.test(ordenado$Valor, ordenado$Tipo, p.adjust.method = "bonf")
+cla1 <- c("#1F618D", "#A04000", "#2E86C1", "#AED6F1", "#D35400", "#EDBB99")
+p2 <- ordenado %>% 
+    mutate(Tipo = fct_relevel(Tipo, 
+                              "HibrydC", "HibrydT", "C1", "C2", "T1", "T2")) %>% 
+    ggplot(aes(Tipo, Valor, fill = Tipo)) + 
+    geom_boxplot() + stat_boxplot(geom ='errorbar', width = 0.5, alpha = 0.3) +
+    theme_bw() + scale_fill_manual(values = cla1) +
+    labs(x = "GENOTIPO",
+         y = "ANALITOS",
+         title = paste("ANALITOS", "DE", "FORRAJE", "DE ACUERDO AL GENOTIPO")) +
+    theme(legend.position = "none",
+          plot.title = element_text(hjust = 0.5, color = "gray25",
+                                    size =  18, face = "bold"),
+          axis.title.x = element_text(color = "gray25"),
+          axis.title.y = element_text(color = "gray25"))
+
+cowplot::plot_grid(p1, p2, labels = "AUTO")
